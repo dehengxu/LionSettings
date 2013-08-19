10715f32ad44643cf16de0439934a12e11b825c4
DehengXu <dehengxu@outlook.com>
2013-08-19 15:54:32 +0800

    add SliderCell

---
+++ src/LlamaSettings/TableCells/SliderCell.h
@@ -0,0 +1,13 @@
+//
+//  SliderCell.h
+//  LRSettings
+//
+//  Created by Deheng.Xu on 13-8-19.
+//  Copyright (c) 2013年 Nicholas.Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SliderCell : UITableViewCell
+
+@end
---
+++ src/LlamaSettings/TableCells/SliderCell.m
@@ -0,0 +1,29 @@
+//
+//  SliderCell.m
+//  LRSettings
+//
+//  Created by Deheng.Xu on 13-8-19.
+//  Copyright (c) 2013年 Nicholas.Xu. All rights reserved.
+//
+
+#import "SliderCell.h"
+
+@implementation SliderCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
