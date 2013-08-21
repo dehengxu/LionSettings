e55ba956d3c073fbefcaa43d1ae4da3a5e432e55
NicholasXu <dehengxu@outlook.com>
2013-08-22 00:32:13 +0800

    add target for lib

---
+++ src/LSExample/LionSettingsLibrary/LionSettingsLibrary.h
@@ -0,0 +1,13 @@
+//
+//  LionSettingsLibrary.h
+//  LionSettingsLibrary
+//
+//  Created by NicholasXu on 13-8-22.
+//  Copyright (c) 2013年 Deheng.Xu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LionSettingsLibrary : NSObject
+
+@end
---
+++ src/LSExample/LionSettingsLibrary/LionSettingsLibrary.m
@@ -0,0 +1,13 @@
+//
+//  LionSettingsLibrary.m
+//  LionSettingsLibrary
+//
+//  Created by NicholasXu on 13-8-22.
+//  Copyright (c) 2013年 Deheng.Xu. All rights reserved.
+//
+
+#import "LionSettingsLibrary.h"
+
+@implementation LionSettingsLibrary
+
+@end
