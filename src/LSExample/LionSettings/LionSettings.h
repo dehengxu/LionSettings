74756f7ee31c940297f975cd45220cf2b7c2437c
DehengXu <dehengxu@outlook.com>
2013-10-15 16:52:10 +0800

    add framework target

---
+++ src/LSExample/LionSettings/LionSettings.h
@@ -0,0 +1,14 @@
+//
+//  LionSettings.h
+//  LSExample
+//
+//  Created by Deheng.Xu on 13-10-15.
+//  Copyright (c) 2013年 Deheng.Xu. All rights reserved.
+//
+
+#ifndef LSExample_LionSettings_h
+#define LSExample_LionSettings_h
+
+
+
+#endif
